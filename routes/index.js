a9ad1169daef2abf0fb243ec896f86a34c691fdb
morningsyj <morningsyj@gmail.com>
2016-10-31 13:56:52 -0700

    register mode

--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,7 @@ router.post('/register', function(req, res) {
             return res.render('register', { account : account });
         }
 
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-        });
+        res.redirect('/');
     });
 });
 
